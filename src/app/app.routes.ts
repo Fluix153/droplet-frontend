bdefca44f7a8821ad7a22df918ca7cb0f49d14f3
Jafeth <jafethworren@gmail.com>
2025-10-27 23:41:22 -0500

    feat: add app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { paymentsRoutes } from './payments/presentation/views/payments.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        children: paymentsRoutes,
+    }
+];
