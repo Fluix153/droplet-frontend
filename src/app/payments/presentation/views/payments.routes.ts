8c2e19d50e528896b7a2af587133d46af821ef5e
Jafeth <jafethworren@gmail.com>
2025-10-28 00:22:56 -0500

    feat: add payment.routes.ts

---
+++ src/app/payments/presentation/views/payments.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { PaymentsComponent } from './payments/payments';
+
+export const paymentsRoutes: Routes = [
+    {
+        path: '',
+        component: PaymentsComponent,
+    },
+];
