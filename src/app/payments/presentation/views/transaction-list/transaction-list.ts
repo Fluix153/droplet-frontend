b32bb3fbb2dd06f37afb8665daa10dbe3671bab8
Jafeth <jafethworren@gmail.com>
2025-10-31 16:15:10 -0500

    feat: update transaction-list.ts

--- src/app/payments/presentation/views/transaction-list/transaction-list.ts
+++ src/app/payments/presentation/views/transaction-list/transaction-list.ts
@@ -1,18 +1,43 @@
-import { Component, inject } from '@angular/core';
-import { PaymentsStore } from '../../../application/payments.store';
-import {CurrencyPipe, DatePipe, NgForOf} from "@angular/common";
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { signal, effect, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export interface Transaction {
+    id: number;
+    name: string;
+    date: string;
+    amount: number;
+}
+
+const http = inject(HttpClient);
+
+export const transactions = signal<Transaction[]>([]);
+
+effect(() => {
+    http.get<Transaction[]>('/server/transactions').subscribe({
+        next: data => transactions.set(data),
+        error: err => console.error('Error loading transactions:', err)
+    });
+});
 
 @Component({
     selector: 'app-transaction-list',
+    standalone: true,
+    imports: [CommonModule],
     templateUrl: './transaction-list.html',
-    styleUrls: ['./transaction-list.css'],
-    imports: [
-        CurrencyPipe,
-        DatePipe,
-        NgForOf
-    ]
+    styleUrls: ['./transaction-list.css']
 })
-export class TransactionListComponent {
-    private readonly store = inject(PaymentsStore);
-    readonly transactions = this.store.recentCharges;
+export class TransactionListView {
+    readonly transactions = signal<Transaction[]>([]);
+
+    constructor() {
+        const http = inject(HttpClient);
+        effect(() => {
+            http.get<Transaction[]>('/server/transactions').subscribe({
+                next: data => this.transactions.set(data),
+                error: err => console.error('Error loading transactions:', err)
+            });
+        });
+    }
 }
