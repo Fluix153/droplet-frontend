8016b96cc0927f18370904993d1e23dc89534814
Jafeth <jafethworren@gmail.com>
2025-10-31 16:14:52 -0500

    feat: update payments.routes.ts

--- src/app/payments/presentation/views/payments.routes.ts
+++ src/app/payments/presentation/views/payments.routes.ts
@@ -1,9 +1,24 @@
 import { Routes } from '@angular/router';
-import { PaymentsComponent } from './payments/payments';
 
 export const paymentsRoutes: Routes = [
     {
         path: '',
-        component: PaymentsComponent,
+        loadComponent: () => import('./payments/payments').then(m => m.PaymentsView)
     },
+    {
+        path: 'methods',
+        loadComponent: () => import('./method-selector/method-selector').then(m => m.MethodSelectorView)
+    },
+    {
+        path: 'transactions',
+        loadComponent: () => import('./transaction-list/transaction-list').then(m => m.TransactionListView)
+    },
+    {
+        path: 'settings',
+        loadComponent: () => import('./billing-settings/billing-settings').then(m => m.BillingSettingsView)
+    },
+    {
+        path: 'wallet',
+        loadComponent: () => import('./wallet-panel/wallet-panel').then(m => m.WalletPanelView)
+    }
 ];
