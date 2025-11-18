110e4f253d978eb08480f08dc9aece4dbe971f7c
Bryan Eduardo Barba Estrada <139183573+br14nbe@users.noreply.github.com>
2025-11-18 10:13:25 -0500

    Revert "Feature/payments"

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    imports: [RouterOutlet],
-    templateUrl: './app.html',
-    styleUrls: ['./app.css']
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
 })
 export class App {
-    protected readonly title = signal('master');
+  protected readonly title = signal('master');
 }
--- src/app/payments/infrastructure/methods/payment-methods-response.ts
+++
@@ -1,7 +0,0 @@
-export interface PaymentMethodsResponse {
-    id: string;
-    type: 'visa' | 'paypal' | 'bank-transfer';
-    label: string;
-    lastDigits?: string;
-    isPrimary: boolean;
-}
